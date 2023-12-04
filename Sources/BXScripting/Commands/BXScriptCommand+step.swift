c524312ffc8a44a8d679e40309a4558cf99c7f06
Peter Baumgartner <peter@boinx.com>
2023-12-04 17:18:02 +0100

    Added step command for progress bar and back/forward buttons

---
+++ Sources/BXScripting/Commands/BXScriptCommand+step.swift
@@ -0,0 +1,50 @@
+//**********************************************************************************************************************
+//
+//  BXScriptCommand+step.swift
+//	Adds a step command to BXScriptCommand
+//  Copyright ©2023 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import Foundation
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+extension BXScriptCommand where Self == BXScriptCommand_step
+{
+	public static func step(_ name:String) -> BXScriptCommand
+	{
+		BXScriptCommand_step(label:name)
+	}
+
+	public static func step(_ name:any RawRepresentable<String>) -> BXScriptCommand
+	{
+		BXScriptCommand_step(label:name.rawValue)
+	}
+}
+
+
+/// This command provides a named step that acts as a goto label for the controller buttons and also provides info for the progress bar.
+
+public struct BXScriptCommand_step : BXScriptCommand, BXLabeledScriptCommand
+{
+	public var label:String
+	
+	public var queue:DispatchQueue = .main
+	public var completionHandler:(()->Void)? = nil
+	public weak var scriptEngine:BXScriptEngine? = nil
+	
+	public func execute()
+	{
+		self.queue.async
+		{
+			completionHandler?() // Nothing to be done here, immediately call completionHandler
+		}
+	}
+}
+	
+
+//----------------------------------------------------------------------------------------------------------------------
