8ba708d44cf0815be10f8d57270654cde256eb29
Peter Baumgartner <peter@boinx.com>
2024-01-24 18:55:04 +0100

    Adapted to new displayMessage implementation

--- Sources/BXScripting/Commands/BXScriptCommand+setMessageBackground.swift
+++ Sources/BXScripting/Commands/BXScriptCommand+setMessageBackground.swift
@@ -17,9 +17,14 @@ extension BXScriptCommand where Self == BXScriptCommand_displayMessage
 {
 	/// Creates a command that displays a text message in the specified window.
 
+	public static func setMessageBackground(in view:@escaping @autoclosure ()->NSView?, padding:NSEdgeInsets? = NSEdgeInsets(top:12, left:32, bottom:12, right:32), cornerRadius:CGFloat = 12.0) -> BXScriptCommand
+	{
+		BXScriptCommand_setMessageBackground(view:view, window:{nil}, backgroundPadding:padding, cornerRadius:cornerRadius)
+	}
+
 	public static func setMessageBackground(in window:@escaping @autoclosure ()->NSWindow?, padding:NSEdgeInsets? = NSEdgeInsets(top:12, left:32, bottom:12, right:32), cornerRadius:CGFloat = 12.0) -> BXScriptCommand
 	{
-		BXScriptCommand_setMessageBackground(window:window, backgroundPadding:padding, cornerRadius:cornerRadius)
+		BXScriptCommand_setMessageBackground(view:{nil}, window:window, backgroundPadding:padding, cornerRadius:cornerRadius)
 	}
 }
 
@@ -34,6 +39,7 @@ extension BXScriptCommand where Self == BXScriptCommand_displayMessage
 
 public struct BXScriptCommand_setMessageBackground : BXScriptCommand
 {
+	var view:()->NSView?
 	var window:()->NSWindow?
 	var backgroundPadding:NSEdgeInsets?
 	var cornerRadius:CGFloat = 12.0
@@ -57,22 +63,28 @@ public struct BXScriptCommand_setMessageBackground : BXScriptCommand
 	
 	private func updateLayers()
 	{
-		guard let window = self.window() else { return }
-		guard let view = window.contentView else { return }
+		guard let view = hostView else { return }
 		guard let textLayer = view.sublayer(named:BXScriptCommand_displayMessage.textLayerName) as? CATextLayer else { return }
 
 		let showsBackground = backgroundPadding != nil
 		let padding = backgroundPadding ?? NSEdgeInsets()
-		let pos = textLayer.position
-		let bounds = view.bounds
-		let autoresizingMask = BXScriptCommand_displayMessage.autoresizingMask(for:bounds, position:pos)
-
-
-
+//		let pos = textLayer.position
+//		let bounds = view.bounds
+		let autoresizingMask:CAAutoresizingMask = [] // BXScriptCommand_displayMessage.autoresizingMask(for:bounds, position:pos)
 
 		BXScriptCommand_displayMessage.updateBackgroundLayer(in:view, visible:showsBackground, padding:padding, cornerRadius:cornerRadius, autoresizingMask:autoresizingMask)
 		BXScriptCommand_displayMessage.updateShadowLayer(in:view, autoresizingMask:autoresizingMask)
 	}
+
+
+	/// Returns the view that hosts the message layer
+	
+	private var hostView:NSView?
+	{
+		self.view() ?? window()?.contentView
+	}
+	
+	
 }
 
 
