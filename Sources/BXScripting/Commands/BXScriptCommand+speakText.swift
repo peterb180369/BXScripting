55405a0ef290f849896d98f9c47ef0cba26516ef
Peter Baumgartner <peter@boinx.com>
2024-01-29 14:05:26 +0100

    Bugfix: Improved voice selection - filter by UI language and remove all bad voices

--- Sources/BXScripting/Commands/BXScriptCommand+speakText.swift
+++ Sources/BXScripting/Commands/BXScriptCommand+speakText.swift
@@ -164,17 +164,64 @@ extension AVSpeechSynthesisVoice
 	
 	static var bestAvailableVoice:AVSpeechSynthesisVoice?
 	{
+		// Get current UI language
+		
 		let preferredLanguages = Bundle.main.preferredLocalizations
-		let code = preferredLanguages.first ?? Self.currentLanguageCode()
-	
+		let language = preferredLanguages.first ?? Self.currentLanguageCode()
+		
+		// Older apps may stil be using "English.lproj" instead of "en.lproj". Convert "English" to "en"
+		// to make sure that the following filtering works correctly.
+		
+		let code = Self.fixLanguageCode(for:language)
+
+		// Filter installed system voices to currently running UI language, sorted by quality
+		
 		let voices = Self.speechVoices()
-			.filter { $0.language.contains(code) }
-			.sorted { $0.quality.rawValue < $1.quality.rawValue }
+			.filter { $0.language.contains(code) }							// remove all voices that do not match current UI language
+			.filter { !$0.identifier.contains("eloquence") } 				// remove all "Eloquence" voices (they are really bad)
+			.filter { !$0.identifier.contains("speech.synthesis.voice") } 	// remove all "Novelty" voices (they are really bad)
+			.sorted { $0.quality.rawValue < $1.quality.rawValue }			// sort by quality
+		
+		
+		voices.forEach
+		{
+			print("\($0.identifier), \($0.gender), \($0.quality)")
+		}
+		
+		// Use the highest quality voice that is available
 		
 		return voices.last
+	}
+	
+	
+	/// Some old apps use English.lproj or German.lproj folders instead of en.lproj and de.lproj. This function converts old style language names
+	/// to modern ISO  language codes.
+	
+	static func fixLanguageCode(for oldName:String) -> String
+	{
+		guard oldName.count > 2 else { return oldName }
+		
+		let name2code:[String:String] =
+		[
+			"Arabic": "ar",
+			"Chinese": "zh",
+			"Dutch": "nl",
+			"English": "en",
+			"French": "fr",
+			"German": "de",
+			"Italian": "it",
+			"Japanese": "ja",
+			"Korean": "ko",
+			"Portuguese": "pt",
+			"Russian": "ru",
+			"Spanish": "es",
+			"Swedish": "sv",
+		]
+		
+		let code = name2code[oldName]
+		return code ?? "en"
 	}
 }
 
  
 //----------------------------------------------------------------------------------------------------------------------
-
